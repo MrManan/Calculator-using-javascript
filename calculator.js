54c8f35ebb27c1b5b3c5a5e0a0b957ab2d207041
MrManan <mrmanan5657@gmail.com>
2021-06-13 15:24:59 +0500

    calculator by using bootstrap

---
+++ calculator.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById('get-result');
+btn.onclick = function() {
+    var firstvalue = document.getElementById('first-name');
+    var secondvalue = document.getElementByI('second-value');
+    var yourresult = document.getElementById('your-result');
+
+}
