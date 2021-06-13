51a2640a63146a57702cdf3d2331125490fbc16a
MrManan <mrmanan5657@gmail.com>
2021-06-13 16:18:34 +0500

    Javascript-added-into-index.html

--- calculator.js
+++ calculator.js
@@ -1,7 +1,30 @@
 var btn = document.getElementById('get-result');
 btn.onclick = function() {
-    var firstvalue = document.getElementById('first-name');
-    var secondvalue = document.getElementByI('second-value');
-    var yourresult = document.getElementById('your-result');
+    var firstvalue = document.getElementById('first-value');
+    var secondvalue = document.getElementById('second-value');
+    var operator = document.getElementById('operator').value;
+    if (operator === "+") {
+        var result = document.getElementById("result");
+        var r = parseInt(firstvalue.value) + parseInt(secondvalue.value);
+        result.value = r;
 
+    }
+    if (operator === "-") {
+        var result = document.getElementById("result");
+        var r = parseInt(firstvalue.value) - parseInt(secondvalue.value);
+        result.value = r;
+
+    }
+    if (operator === "*") {
+        var result = document.getElementById("result");
+        var r = parseInt(firstvalue.value) * parseInt(secondvalue.value);
+        result.value = r;
+
+    }
+    if (operator === "/") {
+        var result = document.getElementById("result");
+        var r = parseFloat(firstvalue.value) / parseFloat(secondvalue.value);
+        result.value = r;
+
+    }
 }
